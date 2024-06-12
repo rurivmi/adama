50a5588d7d0f6279b5159b3d5d1ecf7568e7c6fe
rurivmi <161166358+rurivmi@users.noreply.github.com>
2024-06-12 18:10:52 +0200

    Update intro.py

--- intro.py
+++ intro.py
@@ -7,24 +7,24 @@ st.title('🚗🚗☁️☁️ ADAMA ☁️☁️🏃🏃‍♂️')
 
 
 
-with st.expander('About this app'):
-  st.markdown('**What can this app do?**')
-  st.info('This app allow users to build a machine learning (ML) model in an end-to-end workflow. Particularly, this encompasses data upload, data pre-processing, ML model building and post-model analysis.')
+# with st.expander('About this app'):
+#   st.markdown('**What can this app do?**')
+#   st.info('This app allow users to build a machine learning (ML) model in an end-to-end workflow. Particularly, this encompasses data upload, data pre-processing, ML model building and post-model analysis.')
 
-  st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, go to the sidebar and 1. Select a data set and 2. Adjust the model parameters by adjusting the various slider widgets. As a result, this would initiate the ML model building process, display the model results as well as allowing users to download the generated models and accompanying data.')
+#   st.markdown('**How to use the app?**')
+#   st.warning('To engage with the app, go to the sidebar and 1. Select a data set and 2. Adjust the model parameters by adjusting the various slider widgets. As a result, this would initiate the ML model building process, display the model results as well as allowing users to download the generated models and accompanying data.')
 
-  st.markdown('**Under the hood**')
-  st.markdown('Data sets:')
-  st.code('''- Drug solubility data set
-  ''', language='markdown')
+#   st.markdown('**Under the hood**')
+#   st.markdown('Data sets:')
+#   st.code('''- Drug solubility data set
+#   ''', language='markdown')
   
-  st.markdown('Libraries used:')
-  st.code('''- Pandas for data wrangling
-- Scikit-learn for building a machine learning model
-- Altair for chart creation
-- Streamlit for user interface
-  ''', language='markdown')
+#   st.markdown('Libraries used:')
+#   st.code('''- Pandas for data wrangling
+# - Scikit-learn for building a machine learning model
+# - Altair for chart creation
+# - Streamlit for user interface
+#   ''', language='markdown')
 
 
 
@@ -48,6 +48,4 @@ st.page_link("https://datos.madrid.es/portal/site/egob/menuitem.c05c1f754a33a9fb
 st.page_link("https://datos.madrid.es/portal/site/egob/menuitem.c05c1f754a33a9fbe4b2e4b284f1a5a0/?vgnextoid=41e01e007c9db410VgnVCM2000000c205a0aRCRD&vgnextchannel=374512b9ace9f310VgnVCM100000171f5a0aRCRD", label = "(2) Datos de polución")
 st.page_link("https://datos.madrid.es/portal/site/egob/menuitem.c05c1f754a33a9fbe4b2e4b284f1a5a0/?vgnextoid=02f2c23866b93410VgnVCM1000000b205a0aRCRD&vgnextchannel=374512b9ace9f310VgnVCM100000171f5a0aRCRD", label = "(3) Datos de tráfico")
 st.page_link("https://datos.madrid.es/portal/site/egob/menuitem.c05c1f754a33a9fbe4b2e4b284f1a5a0/?vgnextoid=9f710c96da3f9510VgnVCM2000001f4a900aRCRD&vgnextchannel=374512b9ace9f310VgnVCM100000171f5a0aRCRD&vgnextfmt=default", label = "(4) Festivos comunidad de Madrid")
-st.page_link("https://docs.ultralytics.com/", label = "(5) YOLOv8")
-st.page_link("https://www.skylinewebcams.com/webcam/espana/comunidad-de-madrid/madrid/calle-alcala.html", label = "(6) Webcam Madrid")
 
