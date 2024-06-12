d4122d1cb24e4a0e0944d6cbf6d64ceef4c3656c
rurivmi <161166358+rurivmi@users.noreply.github.com>
2024-06-12 18:17:48 +0200

    Update 03_modelo_imagen.py

--- pages/03_modelo_imagen.py
+++ pages/03_modelo_imagen.py
@@ -6,6 +6,10 @@ import os
 st.header('Modelo imagen')
 st.caption('Detección automática de vehículos mediante YOLO.')
 
+# Obtener la ruta del directorio del script actual
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+
 video_url = "/data/vehicles_detector.mp4"
  
 st.video(video_url)
