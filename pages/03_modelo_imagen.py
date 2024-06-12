e590f18e868bc5b5fbf690366c3c24e5dfa49866
rurivmi <161166358+rurivmi@users.noreply.github.com>
2024-06-12 18:22:05 +0200

    Update 03_modelo_imagen.py

--- pages/03_modelo_imagen.py
+++ pages/03_modelo_imagen.py
@@ -14,5 +14,13 @@ script_dir = os.path.dirname(script_path)
 base_dir = os.path.dirname(script_dir)  # Subir un nivel desde 'pages'
 
 model_path = os.path.join(base_dir, "pickle_modelo", "xgboost_NO2.pkl")
+video_path = os.path.join(base_dir, "data", "vehicles_detector.mp4")
 
-st.video(os.path.join(base_dir, "data", "vehicles_detector.mp4"))
+# Incrustar el video en HTML para reproducir automáticamente y en bucle
+video_html = f"""
+    <video width="700" autoplay loop>
+        <source src="data:video/mp4;base64,{base64.b64encode(open(video_path, 'rb').read()).decode()}" type="video/mp4">
+    </video>
+    """
+
+st.markdown(video_html, unsafe_allow_html=True)
