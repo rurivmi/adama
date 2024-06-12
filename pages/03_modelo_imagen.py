9e6c4851e78a75f1e084317ac46c9b6807f9eca4
rurivmi <161166358+rurivmi@users.noreply.github.com>
2024-06-12 18:42:36 +0200

    Update 03_modelo_imagen.py

--- pages/03_modelo_imagen.py
+++ pages/03_modelo_imagen.py
@@ -17,11 +17,19 @@ base_dir = os.path.dirname(script_dir)  # Subir un nivel desde 'pages'
 model_path = os.path.join(base_dir, "pickle_modelo", "xgboost_NO2.pkl")
 video_path = os.path.join(base_dir, "data", "vehicles_detector.mp4")
 
-# Incrustar el video en HTML para reproducir automáticamente y en bucle
-video_html = f"""
-    <video width="700" autoplay loop>
-        <source src="data:video/mp4;base64,{base64.b64encode(open(video_path, 'rb').read()).decode()}" type="video/mp4">
-    </video>
-    """
+# Comprobar si el archivo de video existe
+if os.path.exists(video_path):
+    # Leer el video y convertirlo a base64
+    with open(video_path, 'rb') as video_file:
+        video_bytes = video_file.read()
+        encoded_video = base64.b64encode(video_bytes).decode()
 
-st.markdown(video_html, unsafe_allow_html=True)
+    # Incrustar el video en HTML para reproducir automáticamente y en bucle, sin sonido
+    video_html = f"""
+        <video width="700" autoplay loop muted>
+            <source src="data:video/mp4;base64,{encoded_video}" type="video/mp4">
+        </video>
+        """
+    st.markdown(video_html, unsafe_allow_html=True)
+else:
+    st.error(f"No se encontró el archivo de video en la ruta: {video_path}")
